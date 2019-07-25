7484ddc23113d8d20bd0c1194422e616822c14e9
Mark <mverleg.noreply@gmail.com>
2019-07-26 01:45:45 +0200

    Fix a bunch of compile errors

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-use ::std::time::Instant;
-
 use crate::naive::solve_naive;
-use crate::util::read_data;
+use crate::util::{read_data, bench};
 use crate::xsort::xsort_par;
+use crate::boxing::boxing_ser;
 
 mod util;
 mod naive;
@@ -12,33 +11,19 @@ mod boxing;
 
 fn main() {
     println!("Welcome to Rust gym 1");
-    const REPS: u128 = 20;
-    let mut total_ms = 0.0;
-    let mut now = Instant::now();
-    let calculate_reference = false;
-    let expected_dist2 = if calculate_reference {
-        println!("solving brute-force to find reference distance");
-        let points = read_data();
-        let (refp1, refp2) = solve_naive(&points);
-        let expected_dist1 = refp1.dist2(&refp2).sqrt();
-        // expected minimum distance 430.809 found in 40833 ms
-        println!("expected minimum distance {0:.3} found in {1:} ms",
-                 expected_dist1.sqrt(), now.elapsed().as_nanos() as f64 / 1_000_000.0);
-        expected_dist1
-    } else {
-        430.80863789034777
-    };
-    for rep in 0..REPS {
-        let mut points = read_data();
-        now = Instant::now();
-        let (p1, p2) = xsort_par(&mut points);
-        let duration = now.elapsed().as_nanos() as f64 / 1_000_000.0;
-        total_ms += duration;
-        let actual_dist1 = p1.dist2(&p2).sqrt();
-        println!("rep {0:}: distance {1:.3} in {2:.3} ms",
-                 rep, actual_dist1, duration);
-        assert!(actual_dist1 < expected_dist2 * 1.000001, "not the shortest distance");
-    }
-    println!("took {0:.3} ms for {1:} points", total_ms / (REPS as f64), read_data().len())
+//    let calculate_reference = false;
+//    let expected_dist2 = if calculate_reference {
+//        println!("solving brute-force to find reference distance");
+//        let points = read_data();
+//        let (refp1, refp2) = solve_naive(&points);
+//        refp1.dist2(&refp2)
+//        // expected minimum distance 430.809 found in 40833 ms
+////        println!("expected minimum distance {0:.3} found in {1:} ms",
+////                 expected_dist1.sqrt(), now.elapsed().as_nanos() as f64 / 1_000_000.0);
+//    } else {
+//        430.80863789034777
+//    };
+    bench("xsort_par", xsort_par, 20);
+    bench("boxing_ser", boxing_ser, 20);
 }
 
--- src/util.rs
+++ src/util.rs
@@ -1,4 +1,5 @@
 use ::std::fs;
+use std::time::Instant;
 
 
 pub type CoordType = f64;
@@ -39,6 +40,8 @@ impl Minimum {
     }
 }
 
+pub type SolveFun = fn(&mut [Point]) -> (Point, Point);
+
 pub fn read_data() -> Vec<Point> {
     let mut points = Vec::with_capacity(100_000);
     let csv = fs::read_to_string("test_data.csv").unwrap();
@@ -56,4 +59,23 @@ pub fn read_data() -> Vec<Point> {
         points.push(point);
     }
     points
+}
+
+pub fn bench(name: &str, solve_fun: SolveFun, reps: usize) {
+    let mut total_ms = 0.0;
+    let mut now;
+    let expected_dist2 = 430.80863789034777;
+    for rep in 0..reps {
+        let mut points = read_data();
+        now = Instant::now();
+        let (p1, p2) = solve_fun(&mut points);
+        let duration = now.elapsed().as_nanos() as f64 / 1_000_000.0;
+        total_ms += duration;
+        let actual_dist1 = p1.dist2(&p2).sqrt();
+        println!("{3:} rep {0:}: distance {1:.3} in {2:.3} ms",
+                 rep, actual_dist1, duration, name);
+        assert!(actual_dist1 < expected_dist2 * 1.000001, "not the shortest distance");
+    }
+    println!("{2:} took {0:.3} ms for {1:} points",
+             total_ms / (reps as f64), read_data().len(), name)
 }
